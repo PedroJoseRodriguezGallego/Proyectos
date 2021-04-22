27122939f51722f8d8053422036877bc5289a58a
Pedro Jose Rodriguez Gallego <prodriguezgallego@safareyes.es>
2021-04-22 13:39:50 +0200

    Eliminada key del django

--- Python/djangoFCT/djangoFCT/settings.py
+++ Python/djangoFCT/djangoFCT/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')h+j9*z#yx+lfx7m#v1ij$7iv@jfd(g$x5jdn=@wdv%2l!ezcf'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
